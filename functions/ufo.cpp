e741a52afe66a6ae9e21c8db26c31b7ce99d50d8
raptor1924 <47617952+raptor1924@users.noreply.github.com>
2022-01-05 22:38:58 -0500

    Update ufo.cpp

--- functions/ufo.cpp
+++ functions/ufo.cpp
@@ -7,6 +7,7 @@ int main()
   //
   std::string codeword, answer;
   std::vector<char> incorrect;
+  char letter;
   bool guess;
   int misses, i;
   //
@@ -22,10 +23,30 @@ greet();
 //
 while (answer != codeword && misses < 7)
 {
+  std::cout << "\033[2J\033[1;1H";
   display_misses(misses);
   display_status(incorrect, answer);
-
-  misses++;
+  std::cout << "Please enter your guess: ";
+  std::cin >> letter;
+  for (i = 0; i < codeword.size(); i++)
+  {
+    if (letter == codeword[i])
+    {
+      answer[i] = letter;
+      guess = true;
+    }
+  }
+  if (guess == true)
+  {
+    std::cout <<  "Correct!";
+  }
+  else
+  {
+    std::cout << "Incorrect!  The tractor beam piulls the purson in further.";
+    incorrect.push_back(letter);
+    misses++;
+  }
+guess = false;
 }
 
 end_game(answer, codeword);
