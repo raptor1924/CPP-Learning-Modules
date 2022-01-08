3522447756948e8fa48439b2b7363013107ae2f5
raptor1924 <47617952+raptor1924@users.noreply.github.com>
2022-01-07 19:56:50 -0500

    Header file for classes demo

---
+++ Classes/song.hpp
@@ -0,0 +1,14 @@
+#include <string>
+
+// add the Song class here:
+class Song
+{
+  //Properties
+  //
+  std::string title;
+  //Methods
+  //
+  public:
+  void add_title(std::string new_title);
+  std::string get_title();
+};
