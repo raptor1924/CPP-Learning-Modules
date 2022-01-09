a1fcb69948a48b9a9c3369ec3f8392395c25f364
raptor1924 <47617952+raptor1924@users.noreply.github.com>
2022-01-09 16:26:31 -0500

    Add declarations for artist set and retrieve

--- Classes2/song.hpp
+++ Classes2/song.hpp
@@ -6,7 +6,13 @@ class Song {
   std::string artist;
 
 public:
+  //Set and retrieve song title
+  //
   void add_title(std::string new_title);
   std::string get_title();
+  //Set and retrieve song artist
+  //
+  void add_artist(std::string new_artist);
+  std::string get_artist();
   
 };
