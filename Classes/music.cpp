f649eb9b93fb58192be36949328dac42fdcdc347
raptor1924 <47617952+raptor1924@users.noreply.github.com>
2022-01-09 16:13:18 -0500

    Song Class Demo

--- Classes/music.cpp
+++ Classes/music.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include "song.hpp"
 
-int main() {
-
+int main() 
+{
+  
+  Song electric_relaxation;
+  electric_relaxation.add_title("Electric Relaxation");
+  std::cout << electric_relaxation.get_title();
+  
 }
