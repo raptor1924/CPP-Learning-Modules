cefde0e74f229d7b969a275bfc91fb014a5f9ff7
raptor1924 <47617952+raptor1924@users.noreply.github.com>
2022-01-09 16:25:20 -0500

    Set and retrieve object data update

--- Classes2/music.cpp
+++ Classes2/music.cpp
@@ -4,5 +4,8 @@
 int main() {
 
   Song electric_relaxation;
-  
+  //Set and retrieve song artist
+  //
+  electric_relaxation.add_artist("A Tribe Called Quest");
+  std::cout << electric_relaxation.get_artist();
 }
