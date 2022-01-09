174f586334383a468121d8638ec60012267154d7
raptor1924 <47617952+raptor1924@users.noreply.github.com>
2022-01-09 16:25:58 -0500

    Class functions updated
    Add code to set and retrieve artist object data

--- Classes2/song.cpp
+++ Classes2/song.cpp
@@ -1,14 +1,28 @@
 #include "song.hpp"
 
 // add Song method definitions here:
-void Song::add_title(std::string new_title) {
-
+//
+//Add song title to object
+//
+void Song::add_title(std::string new_title) 
+{
   title = new_title;
-
 }
-
-std::string Song::get_title() {
-
+//Retrieve song title from object
+//
+std::string Song::get_title() 
+{
   return title;
-
+}
+//Add song artist to object
+//
+void Song::add_artist(std::string new_artist)
+{
+  artist = new_artist;
+}
+//Retrieve song artist from object
+//
+std::string Song::get_artist()
+{
+  return artist;
 }
