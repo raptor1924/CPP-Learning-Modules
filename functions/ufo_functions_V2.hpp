08655a96af38eb4f166125578e8e8389b9b6982d
raptor1924 <47617952+raptor1924@users.noreply.github.com>
2022-03-12 14:40:51 -0500

    Update ufo_functions_V2.hpp

--- functions/ufo_functions_V2.hpp
+++ functions/ufo_functions_V2.hpp
@@ -1,7 +1,8 @@
 #include <vector>
+//
 // Declare functions
 //
 void greet();
 void display_misses(int misses);
 void display_status(std::vector<char> incorrect, std::string answer);
-void end_game(std::string answer, std::string codeword);
+void end_game(std::string codeword, std::string answer);
